69efb5071df83fe3f0a02af1724122e7a90f40e2
vpanyushenko <vpanyushenko@outlook.com>
2022-12-27 06:23:34 -0800

    Add test

---
+++ src/routes/chart/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+	// eslint-disable svelte/no-at-html-tags
+	import { Position } from '@unovis/ts';
+	import { VisAxis, VisCrosshair, VisStackedBar, VisTooltip, VisXYContainer } from '@unovis/svelte';
+	import { data, labels } from './data';
+
+	const height = 450;
+	const x = (d) => d.year;
+	const y = labels.map((l) => (d) => d[l.format]);
+
+	function getIcon(f) {
+		return `<span class="bi bi-${f.icon}" style="color:${f.color}; margin:0 4px"></span>${f.label}\t`;
+	}
+
+	function tooltipTemplate(d) {
+		const numberFormat = Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'USD',
+			notation: 'compact'
+		}).format;
+		const dataLegend = labels
+			.filter((f) => d[f.format] > 0)
+			.reverse()
+			.map((f) => `<span>${getIcon({ ...f, label: numberFormat(d[f.format] * Math.pow(10, 10)) })}`)
+			.join('</span>');
+		return `<div><b>${d.year}</b>: ${dataLegend}</div>`;
+	}
+</script>
+
+<link
+	rel="stylesheet"
+	href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+/>
+<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+
+<h2>
+	When you navigate back to home, there will be an issue in the console and the navigation breaks
+</h2>
+
+<VisXYContainer {data} {height}>
+	<VisStackedBar {x} {y} />
+	<VisCrosshair template={tooltipTemplate} />
+	<VisTooltip verticalShift={height} horizontalPlacement="center" />
+	<VisAxis type="x" />
+</VisXYContainer>
